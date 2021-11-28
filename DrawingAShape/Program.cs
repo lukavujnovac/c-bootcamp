8166ef275482b26998327c2e5c5eae098ecc672a
Luka <vujnovacluka89@gmail.com>
2021-11-28 15:07:18 +0100

    variable basics completed

---
+++ DrawingAShape/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DrawingAShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really liked the name" + characterName);
+            Console.WriteLine("But he didn't like being " + characterAge + " years old");
+
+            Console.ReadLine();
+        }
+    }
+}
